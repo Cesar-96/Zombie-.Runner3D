eca71907427fe2b17c735af15f8649ce905694c0
Pablo Yucra <cesar.yucra12@gmail.com>
2019-08-21 06:40:08 -0500

    terreno and more

---
+++ Zombie Runner/Assets/Scripts/RadioSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSystem : MonoBehaviour
+{
+
+	public AudioClip initialHelicall;
+	public AudioClip initialCallReply;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnMakeInitialHeliCall()
+	{
+		print(name + "OnMakeInitialHeliCall");
+		audioSource.clip = initialHelicall;
+		audioSource.Play();
+		Invoke("InitialReply", initialHelicall.length + 1f);
+	}
+
+	void InitialReply()
+	{
+		audioSource.clip = initialCallReply;
+		audioSource.Play();
+		BroadcastMessage("OnDispatchHelicopter");
+	}
+
+}
